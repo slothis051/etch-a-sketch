31e55afe6e2dc1d8953dfb52d548e0c3d4e570a9
slothis <chenhangoh051@gmail.com>
2023-11-06 23:02:32 +0800

    Added input bar for grid size adjustment

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,25 +1,48 @@
 const main = document.querySelector('#main');
+const inputBar = document.querySelector('#inputBar');
+let warningMessage = document.querySelector("#warning");
+let gridNum = 16;
 
-for(let i=0; i<16; i++) {
-    let mainRow = document.createElement('div');
-    mainRow.style = "display: flex; margin: -1px 0px; flex: 1 0 50px;"
-    main.appendChild(mainRow);
-    for(let j=0; j<16; j++) {
-        let mainCol = document.createElement('div');
-        mainCol.style = "border-style: solid; flex: 0 1 50px;"
-        mainCol.classList.add("box");
-        mainRow.appendChild(mainCol);
-    }
+
+let inputButton = document.querySelector('#inputButton');
+if (inputBar) {
+    inputButton.addEventListener('click', ()=>{
+        createGrid(inputBar.value);
+    } );
 }
 
-let boxes = document.querySelectorAll('.box');
-boxes.forEach(box => {
-    box.addEventListener('mouseover', () => {
-        box.style.background = "pink";
-    });
-    box.addEventListener('mouseout', () => {
-        setTimeout( () => {
-            box.style.background = "transparent";
-        }, 250);
-    });
-});
+function createGrid(gridSize){
+    while (main.firstChild) {
+        main.removeChild(main.lastChild);
+    }
+    if (gridSize <= 100) {
+        for(let i=0; i<gridSize; i++) {
+            let mainRow = document.createElement('div');
+            mainRow.classList.add('mainRow');
+            main.appendChild(mainRow);
+            for(let j=0; j<gridSize; j++) {
+                let mainCol = document.createElement('div');
+                mainCol.classList.add("box");
+                mainCol.style = `flex: 0 1 ${720/gridSize}px`;
+                mainRow.appendChild(mainCol);
+            }
+        }
+        draw();
+    } else {
+        warningMessage.textContent = "Warning! Grid size should not exceed 100!";
+    }        
+
+    function draw(){
+        let boxes = document.querySelectorAll('.box');
+            boxes.forEach(box => {
+                box.addEventListener('mouseover', () => {
+                    box.style.background = "pink";
+                });
+                box.addEventListener('mouseout', () => {
+                    setTimeout( () => {
+                        box.style.background = "transparent";
+                    }, 250);
+                });
+            });
+    }
+}
